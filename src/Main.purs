84582533fe649952fc9262768c433729a4af3465
Peter Vilja <peter.vilja@gmail.com>
2017-05-16 09:16:28 +0300

    add an example of functor composition law

--- src/Main.purs
+++ src/Main.purs
@@ -6,6 +6,8 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Argonaut.Core (Json)
 import Data.Argonaut.Parser (jsonParser)
 import Data.Either (Either)
+import Data.Maybe (Maybe(Just))
+import Data.String (toUpper, trim)
 
 userData :: String
 userData = "[{\"name\": \"Tom\", \"age\": 25}, {\"name\": \"John\", \"age\": 30}]"
@@ -13,6 +15,17 @@ userData = "[{\"name\": \"Tom\", \"age\": 25}, {\"name\": \"John\", \"age\": 30}
 parseUsers :: String -> Either String Json
 parseUsers users = jsonParser users
 
+-- Simple usage of Maybe
+-- main :: forall e. Eff (console :: CONSOLE | e) Unit
+-- main = do
+--   log $ show $ parseUsers userData
+
+
+
+
+-- functor should preserve composition morphisms
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log $ show $ parseUsers userData
+  -- log $ show $ map (toUpper <<< trim) $ Just(" john ")
+  log $ show $ map toUpper <<< map trim $ Just(" john ")
+  -- log $ show $ map toUpper (map trim) $ Just(" john ")
